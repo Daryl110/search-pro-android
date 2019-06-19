69898c93028cc94e54fcf5b816bdc8e930e9dc99
daryl110 <daryl.8220@gmail.com>
2019-06-18 22:07:17 -0500

    Diseñando el login

--- app/src/main/java/com/search/Controlador/MainActivity.java
+++ app/src/main/java/com/search/Controlador/MainActivity.java
@@ -1,8 +1,10 @@
 package com.search.Controlador;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick_Registrar(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Hola Mundo").setTitle("Mensaje");
+
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
     }
 }
